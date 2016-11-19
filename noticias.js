0b7520759a849058ed95e8c3f7409d0ba3462ca2
Bruno Justino <ti@qualconcurso.com.br>
2016-11-19 17:29:44 -0200

    Using if statement to request a new url

--- noticias.js
+++ noticias.js
@@ -1,6 +1,24 @@
 var http = require('http');
 
-http.createServer(function(req,res){
+var server = http.createServer(function(req,res){
 
-	res.end("<html><body>Portal de Noticias</body></html>");
-}); 
+	var categoria = req.url;
+
+	if (categoria == '/tecnologia'){
+
+		res.end("<html><body>Noticias de tecnlogia</body></html>");
+
+	}else if (categoria=='/moda'){
+
+		res.end("<html><body>Noticias de Moda</body></html>");
+
+	}else if (categoria=='/beleza'){
+
+		res.end("<html><body>Noticias de Beleza</body></html>");
+	}else{
+
+		res.end("<html><body>Portal de Noticias</body></html>");	
+	}
+});
+
+server.listen(3000);
