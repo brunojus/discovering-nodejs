f2a743aeed527d79fea75b41a8f56e6901f5d435
Bruno Justino <ti@qualconcurso.com.br>
2016-11-25 23:57:57 -0200

    Including a form validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,6 +7,22 @@ module.exports = function(app){
 	app.post('/noticias/salvar',function (req,res) {
 		var noticia = req.body;
 
+		req.assert('titulo','O Título é obrigatório').notEmpty();
+		req.assert('resumo','O Resumo é obrigatório').notEmpty();
+		req.assert('resumo','O Resumo deve conter entre 10 e 100 caracteres').len(10,100);
+		req.assert('autor','O autor é obrigatório').notEmpty();
+		req.assert('data_noticia','Data é obrigatória').notEmpty().isDate({format:'YYYY-MM-DD'});
+		req.assert('noticia','O campo noticia é obrigatório').notEmpty();
+
+		var erros = req.validationErrors();
+
+		if(erros){
+
+			res.render('admin/form_add_noticia',{validacao:erros});
+			return;
+		}
+
+
 
 		var connection = app.config.dbConnection();
 		var Noticias = new app.app.models.Noticias(connection);
